164756fbdc9206760d0616b0b60b8dc1cb9d68fd
Dennis Ameling <dennis@dennisameling.com>
2021-07-16 13:28:24 +0200

    Allow setting VCPKG_ROOT for the toolchain

--- triplets/community/x64-windows-llvm-mingw.cmake
+++ triplets/community/x64-windows-llvm-mingw.cmake
@@ -3,11 +3,16 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_ENV_PASSTHROUGH PATH)
 set(VCPKG_ENV_PASSTHROUGH LLVM_PATH)
+set(VCPKG_ENV_PASSTHROUGH VCPKG_ROOT)
 SET(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
 
 set(VCPKG_CMAKE_SYSTEM_NAME LLVM_MinGW)
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "F:/repos/vcpkg/scripts/toolchains/llvm-mingw.cmake")
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/toolchains/llvm-mingw.cmake")
 
 if(NOT DEFINED ENV{LLVM_PATH})
-    message(FATAL_ERROR "LLVM_PATH env variable is not set. Make sure to set it, together with VCPKG_KEEP_ENV_VARS=\"LLVM_PATH\"")
+    message(FATAL_ERROR "LLVM_PATH env variable is not set. Make sure to set it, and use forward slashes in the path")
+endif()
+
+if(NOT DEFINED ENV{VCPKG_ROOT})
+message(FATAL_ERROR "VCPKG_ROOT env variable is not set. Make sure to set it, and use forward slashes in the path")
 endif()
